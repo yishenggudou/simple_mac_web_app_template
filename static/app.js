57620d296d495a079d0624cfb2e4b22ff91810bb
愧僧 <kuiseng.zhb@alibaba-inc.com>
2020-02-17 14:33:35 +0800

    add
    add

--- static/app.js
+++ static/app.js
@@ -1,1 +1,40 @@
-$(document).find('[target=_blank]').attr('target', 'none')
+(function () {
+    class TimgerApp {
+        static getInstance(name) {
+            if (!this.instance) {
+                this.instance = new TimgerApp(name);
+            }
+            return this.instance;
+        }
+
+        constructor(name) {
+            this.name = name;
+            this.instance = null;
+        }
+
+        setupAd() {
+
+        }
+
+        setupTrace() {
+
+        }
+
+        setupLink() {
+            setInterval(function () {
+                $(document).find('[target=_blank]').attr('target', 'none')
+            }, 1000)
+        }
+    }
+
+    window.onload = function () {
+        if (window.jQuery) {
+
+            console.log("jquery has installed!");
+        } else {
+            // jQuery is not loaded
+            console.log("jquery has installed!");
+
+        }
+    };
+})()
