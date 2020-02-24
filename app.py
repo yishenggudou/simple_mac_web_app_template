07f0139559b4059c24623ca45e42b492702865cd
愧僧 <kuiseng.zhb@alibaba-inc.com>
2020-02-24 12:00:15 +0800

    add
    wechat

--- app.py
+++ app.py
@@ -43,13 +43,13 @@ DIR = os.path.abspath(os.path.dirname(__file__))
 """
 This example demonstrates how to create a webview window.
 """
-icon_path = os.path.join(DIR, 'icons/yinxiang.icns')
+icon_path = os.path.join(DIR, 'icons/wechat.icns')
 if __name__ == '__main__':
     # Create a standard webview window
-    window = webview.create_window('印象笔记', 'https://app.yinxiang.com/Home.action',
-     width=600,
+    window = webview.create_window('wechat', 'https://wx.qq.com/',
+     width=400,
      height=800,
     )
     window.loaded += on_loaded
-    #webview.start(evaluate_js,window,debug=True,)
-    webview.start(debug=True,)
+    #webview.start(evaluate_js,window,)
+    webview.start()
