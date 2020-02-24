fc122c2733cd7e4241fb94713f0973212142ae62
愧僧 <kuiseng.zhb@alibaba-inc.com>
2020-02-24 22:09:40 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 rm -vrf ./.eggs/* build/* dist/*
 python pre_build.py $1
 python setup.py clean py2app
-#ln -s dist/$1.app dist/app.app
-cmd="dmgbuild -s settings.py app dist/$1-install.dmg"
+mv  dist/app.app dist/$1.app
+cmd="dmgbuild -s settings.py $1 dist/$1-install.dmg"
 echo ${cmd}
 $cmd
 open dist/
